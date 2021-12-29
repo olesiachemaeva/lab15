6d4f9e29667ba4addd857ac06633b2f0abccda8b
chemaeva8898@gmail.com <chemaeva8898@gmail.com>
2021-12-29 15:52:46 +1000

    Добавьте файлы проекта.

---
+++ lab15/Program.cs
@@ -0,0 +1,101 @@
+﻿
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            Console.WriteLine("---------Арифметическая прогрессия:");
+            ArithProgression arithProgression = new ArithProgression();
+            arithProgression.setStart(7);
+            arithProgression.setStep(4);
+            Console.WriteLine(arithProgression.getNext());
+            Console.WriteLine(arithProgression.getNext());
+            Console.WriteLine(arithProgression.getNext());
+            arithProgression.reset();
+
+           
+            //ArefmProgr.Funk(3, 2.65, 8);
+            Console.WriteLine("---------Геометрическая прогрессия:");
+
+            GeomProgression geomProgression = new GeomProgression();
+            geomProgression.setStart(7);
+            geomProgression.setStep(4);
+            Console.WriteLine(geomProgression.getNext());
+            Console.WriteLine(geomProgression.getNext());
+            Console.WriteLine(geomProgression.getNext());
+            geomProgression.reset();
+            //GeomertProgr.Funk(2, 9);
+            Console.ReadLine();
+        }
+    }
+    interface ISeries
+    {
+        void setStart(int x);
+        int getNext();
+        void reset();
+    }
+    class ArithProgression : ISeries
+    {
+        int step;
+        int startValue;
+        int currentValue;
+
+        public int getNext()
+        {
+            currentValue += step;
+            return currentValue;
+        }
+
+        public void reset()
+        {
+            currentValue = startValue;
+        }
+
+        public void setStart(int x)
+        {
+            startValue = x;
+            currentValue = startValue;
+        }
+        public void setStep(int s)
+        {
+            step = s;
+        }
+    }
+    class GeomProgression : ISeries
+
+    {
+        int step;
+        int startValue;
+        int currentValue;
+
+        public int getNext()
+        {
+            currentValue *= step;
+            return currentValue;
+        }
+
+        public void reset()
+        {
+            currentValue = startValue;
+        }
+
+        public void setStart(int x)
+        {
+            startValue = x;
+            currentValue = startValue;
+        }
+        public void setStep(int s)
+        {
+            step = s;
+        }
+    }
+}
---
+++ variant 2/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace variant_2
+{
+    namespace ConsoleApplication2
+    {
+        public class Class1
+        {
+
+            static void Main(string[] args)
+            {
+                
+                Console.WriteLine("---------Арифметическая прогрессия:");
+                ArefmProgr.Funk(3, 2.65, 8);
+                Console.WriteLine("---------Геометрическая прогрессия:");
+                GeomertProgr.Funk(2, 9);
+                Console.ReadLine();
+            }
+        }
+        class ArefmProgr // класс арифметической прогрессии
+        {
+            // firstElem - первый элемент последовательности
+            // stepOfElem - шаг последователности
+            // endElem - номер последнего члена прогрессии
+            // ArefmSumElem - сумма n-ных членов прогресии
+
+            public static void Funk(double firstElem, double stepOfElem, int endElem)
+            {
+                double[] ArefmProgrArray = new double[endElem];
+                ArefmProgrArray[0] = firstElem;
+                double ArefmSumElem = firstElem;
+
+                for (int i = 1; i < endElem; i++)
+                {
+                    ArefmProgrArray[i] = ArefmProgrArray[0] + (i - 1) * stepOfElem;
+                    Console.Write(ArefmProgrArray[i] + " ");
+                    ArefmSumElem += ArefmProgrArray[i];
+                }
+                Console.WriteLine("\nХарактеристики: \nпервый член = {0} \nшаг = {1} \nномер последнего члена = {2}", firstElem, stepOfElem, endElem);
+                Console.WriteLine("Sum= " + (ArefmSumElem + firstElem));
+
+            }
+        }
+        public class GeomertProgr // класс геометрическоц прогрессии
+        {
+            // firstElem - первый элемент последовательности
+            // endElem - номер последнего члена прогрессии
+
+            public static void Funk(double firstElem, int endElem)
+            {
+                double[] ArefmProgrArray = new double[endElem];
+                ArefmProgrArray[0] = firstElem;
+                double ArefmSumElem = firstElem;
+
+                for (int i = 1; i < endElem; i++)
+                {
+                    ArefmProgrArray[i] = Math.Pow(ArefmProgrArray[0], i);
+                    Console.Write(ArefmProgrArray[i] + " ");
+                    ArefmSumElem += ArefmProgrArray[i];
+                }
+                Console.WriteLine("\nХарактеристики: \nпервый член = {0}\nномер последнего члена = {1}", firstElem, endElem);
+                Console.WriteLine("Sum= " + (ArefmSumElem + firstElem));
+
+            }
+        }
+
+    }
+}
